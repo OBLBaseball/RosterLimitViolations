526843419383695867c3e5f4a8e2910cb97b3175
Jeffrey <zic.jeffrey@gmail.com>
2017-06-17 21:04:34 -0400

    feat: add Team holder
    Created container to keep track of teams info after reading

--- RosterLimitViolations.py
+++ RosterLimitViolations.py
@@ -5,3 +5,26 @@
 # violating the roster limit rule
 # Date: 6/17/17
 # Author: Jeffrey Zic
+
+
+class Team:
+
+    """A holder for team info"""
+
+    def __init__(self,id,name,rosterSize,level):
+        """Initializes a Team.
+
+        :param id: the team ID, mathces with team name
+        :param name: the team name, matches with team id
+        :param rosterSize: the current number of players on the team
+        :param level: The team's level in the organization (MLB, A, etc.)
+        :type id: int
+        :type name: str
+        :type rosterSize: int
+        :type level: str
+        """
+
+        self.id = id
+        self.name = name
+        self.size =-rosterSize
+        self.level = level
